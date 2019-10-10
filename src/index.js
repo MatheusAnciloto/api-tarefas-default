58ae1da899090a5bac9962ed9e260d89c58de3cf
MatheusAnciloto <ancilotom@gmail.com>
2019-10-09 22:06:24 -0400

    Comitando novas coisas

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const morgan = require('morgan')
+//const morgan = require('morgan')
+const YAML = require('yamljs')
+const swaggerUI = require('swagger-ui-express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-app.use(morgan('combined'))
+
+
+// Habilitando o CORS
+app.use(cors())
+
+//Transformando o corpo da requisição no formato JSON
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+
+//app.use(morgan('combined'))
+
+//Documentação da API
+const swaggerDocument = YAML.load('./docs/swagger.yml')
+app.use('api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 //ROTAS (URN)
 const tarefaRoute = require('./routes/tarefaRoute')
 app.use('/api/v1/tarefas', tarefaRoute)
+const apiRoute = require('./routes/apiRoute')
+app.use('/api/v1', apiRoute)
 
+//Porta da aplicação
 const port = process.env.PORT
 
 app.listen(port, () => {
